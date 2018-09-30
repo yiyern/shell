9d326df3e8ff554a9109b56965cbeb555d514e05
penta <yiyern@yahoo.com>
2018-09-30 15:51:53 +0800

    Create local-docker.sh

---
+++ local-docker.sh
@@ -0,0 +1,15 @@
+sudo yum remove docker docker-common docker-selinux docker-engine
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+# sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+sudo yum clean all
+sudo yum makecache fast
+sudo yum install docker-ce -y
+sudo cat > /etc/docker/daemon.json <<EOF
+{
+"registry-mirrors": ["https://registry.docker-cn.com","https://docker.mirrors.ustc.edu.cn"]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo systemctl enable docker
