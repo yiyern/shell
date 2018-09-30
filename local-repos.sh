54be623c44ea07d37eed53d86753ed3f314c7669
penta <yiyern@yahoo.com>
2018-09-30 15:56:06 +0800

    Update and rename local-source.sh to local-repos.sh

--- local-repos.sh
+++ local-repos.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 cd /etc/yum.repos.d
 sudo mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
 sudo curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
