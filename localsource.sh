8a091d3fdaa34cd15b30318144c108b4fc00736e
penta <yiyern@yahoo.com>
2018-09-30 15:51:21 +0800

    Create localsource.sh

---
+++ localsource.sh
@@ -0,0 +1,5 @@
+cd /etc/yum.repos.d
+sudo mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+sudo curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+sudo yum clean all
+sudo yum makecache fast
