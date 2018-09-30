2d1c1c59b7e3ea967ff487170d01ac96f3ef74f4
penta <yiyern@yahoo.com>
2018-09-30 15:55:42 +0800

    Update local-docker.sh

--- local-docker.sh
+++ local-docker.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 sudo yum remove docker docker-common docker-selinux docker-engine
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 # sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
